9a09ecff203db7b1ff5a1c81c8495a46c7e7fd7e
slava_v <letterthing@gmail.com>
2020-05-13 23:06:24 +0700

    Сверстан хэдер

--- babel.config.js
+++ babel.config.js
@@ -3,11 +3,13 @@ const presets = [
       "@babel/env",
       {
         targets: { // указать цели, для полифилов
-              edge: "17",
-              ie: "11",
-              firefox: "50",
-              chrome: "64",
-              safari: "11.1",
+          android: '67',
+          ios: '12',
+          edge: "17",
+          ie: "11",
+          firefox: "50",
+          chrome: "64",
+          safari: "11.1",
         },
         useBuiltIns: "usage", // эта настройка babel-polyfill, если стоит значение usage, то будут подставлятся полифилы для версий браузеров которые указали ниже.
         corejs: "3.4.1" // явно проставить версию corejs
@@ -16,3 +18,4 @@ const presets = [
   ];
   
   module.exports = { presets };
+  
